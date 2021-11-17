ceccb5d9ca0b91f4c6082913d40cfedf2bccf1ec
yvadher <yagniktechmail@gmail.com>
2021-11-17 01:15:08 -0800

    update apis

--- Backend/app.py
+++ Backend/app.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 import sqlite3
 from sqlite3 import Error
+import datetime
 import flask
 from flask import request
 from flask import jsonify
@@ -26,7 +27,6 @@ def create_connection(db_file):
 
 # Initialize DB
 def initialize_db(conn):
-
 	commands = [("""CREATE TABLE IF NOT EXISTS Users (
 			user_id TEXT PRIMARY KEY,
 			name TEXT,
@@ -56,51 +56,53 @@ def initialize_db(conn):
 
 		# when products out, update products
 		("""CREATE TABLE IF NOT EXISTS SoldItems (
-			product_id INTEGER PRIMARY KEY,
+			sold_item_id INTEGER PRIMARY KEY,
 			product_barcode INTEGER,
-			product_selling_price INTEGER,
 			quantity INTEGER,
-			FOREIGN KEY (product_id) REFERENCES Products(product_id)
+			transaction_id INTEGER,
+			FOREIGN KEY (transaction_id) REFERENCES Transactions(transaction_id)
 		)"""),
 
 		("""CREATE TABLE IF NOT EXISTS Transactions (
 			transaction_id INTEGER PRIMARY KEY,
 			user_id TEXT,
 			date TEXT,
-			transaction_total REAL,
+			total_items INTEGER,
+			total_sale REAL,
 			bills_offered REAL,
 			FOREIGN KEY (user_id) REFERENCES Users(user_id)
-		)""")]
+		)""")
+		]
 	with conn:
 		c = conn.cursor()
 		for command in commands:
 			c.execute(command)
 
-
-def insert_db(conn):
-	with conn:
-		c = conn.cursor()
-		# c.execute("INSERT INTO Users VALUES (?, ?, ?, ?)", ('userid', 'name', 'password'))
-		c.execute("INSERT INTO Products VALUES (123456789, 'Apple', 10, 1, 1,1,1)")
-		# c.execute("INSERT INTO ProductsIn VALUES (?, ?, ?, ?, ?)", (0, 0, 'product', 0, 0, 0))
-		# c.execute("INSERT INTO Transactions VALUES (?, ?, ?, ?, ?)", (0, 'userid', 'date', 0, 0))
-		# c.execute("INSERT INTO SoldItems VALUES (?, ?, ?, ?)", (0, 0, 0, 0))
-
 def get_price_by_barcode(barcode, conn):
 	c = conn.cursor()
-
 	c.execute("SELECT * FROM Products WHERE product_barcode = " + str(barcode))
 	return c.fetchone()
 	
-def set_product(barcode, name, price, tax, conn):
+def set_product(barcode, name, price, tax, total_qty, total_sold_qty, total_instock_qty, conn):
 	c = conn.cursor()
-	try:
-		c.execute("INSERT INTO Products (product_barcode, product_name, product_selling_price, product_tax, product_sold_quantity,product_total_quantity,product_instock_quantity) VALUES (?,?,?,?,?,?,?))",(barcode,name,price,tax,0,0,0))
-		
-		return jsonify(c.fetchall())
-	except Error as ex:
-		return ex
+	c.execute("INSERT OR REPLACE INTO Products (product_barcode, product_name, product_selling_price, product_tax, product_sold_quantity,product_total_quantity,product_instock_quantity) VALUES (?,?,?,?,?,?,?)",(barcode, name, price, tax, total_qty, total_sold_qty, total_instock_qty))
+	conn.commit()
+	return c.lastrowid
 
+
+def set_transaction(user_id, total_sale, total_item, conn):
+	c = conn.cursor()
+	transaction = (user_id, datetime.datetime.utcnow(), total_item, total_sale, 0 )
+	c.execute("INSERT INTO Transactions (user_id, date, total_items, total_sale, bills_offered) VALUES(?,?,?,?,?)", transaction)
+	conn.commit()
+	return c.lastrowid
+
+def set_sold_items(product_barcode, product_selling_price, quantity, transaction_id):
+	c = conn.cursor()
+	sold_item = (product_barcode, quantity)
+	c.execute("INSERT INTO SoldItems (product_barcode, quantity, transaction_id) VALUES(?,?,?)", sold_item)
+	conn.commit()
+	return c.lastrowid
 
 conn = create_connection('retail.db')
 initialize_db(conn)
@@ -112,29 +114,63 @@ initialize_db(conn)
 
 @app.route('/', methods=['GET'])
 def home():
-    return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
+    return "<h1>POS system</h1><p>This site is a prototype API for POS.</p>"
 
 
-@app.route('/getPriceByBarcode', methods=['POST'])
+@app.route('/product', methods=['GET'])
 @cross_origin(origin='localhost',headers=['Content- Type','Authorization'])
 def POST_price_by_barcode():
-    print (request.get_json(), flush=True)
-    with create_connection('retail.db') as conn:
-        return jsonify(get_price_by_barcode(request.get_json()['barcode'], conn))
+	print (request.args.get('barcode'), flush=True)
+	barcode = request.args.get('barcode')
+	if (not barcode):
+		return jsonify({"result": "Pelase provide any detail of the product in query"})
+	try:
+		with create_connection('retail.db') as conn:
+			return jsonify(get_price_by_barcode(barcode, conn))
+	except Error as ex: 
+		return jsonify({"result": ex})
 
-@app.route('/saveProduct', methods=['POST'])
+
+@app.route('/product', methods=['POST'])
+@cross_origin(origin='localhost',headers=['Content- Type','Authorization'])
 def POST_save_product():
-	print (request.get_json(), flush=True)
-	# Validation of data
 	data = request.get_json()
-	with create_connection('retail.db') as conn:
-		return set_product(data["barcode"], data["name"], data["price"], data["tax"], conn)
-	return "Failed"
+	try:
+		with create_connection('retail.db') as conn:
+			result = set_product(data["product_barcode"], data["product_name"], data["product_selling_price"], data["product_tax"], data["product_total_qty"],data["product_sold_qty"], data["product_instock_qty"],conn)
+			return (str(result))
+	except KeyError:
+		return "Make sure you have all keys are correct"
+	except Error as ex:
+		print (ex, flush=True)
+		return "Failed with serverside issue"
+	except:
+		return "unknow issue"
 
 
-
-# NEED FUNCTION FOR SAVING THE PRODUCT 
-# e.g data will be given in json { name: "", id: "", etc. } and just need to insert in db
+@app.route('/order', methods=['POST'])
+def POST_save_order():
+	"""
+	json structure 
+	{
+		"user_id" : "testing",
+		"total_sale": 2,
+		"total_items": 2,
+		"item_list": {
+			"9278349723": 1,
+			"123123": 9
+		}
+	}
+	"""
+	print (request.get_json(), flush=True)
+	data = request.get_json()
+	try:
+		with create_connection('retail.db') as conn:
+			transaction_id = set_transaction(data.user_id, data.total_sale, data.total_items, conn)
+			for barcode,qty in data:
+				print (key + " " + value, flush=True)
+				set_sold_items(barcode, qty, transaction_id, conn)
+	except Error as ex:
+		return jsonify({"result": ex})
     
-
-app.run(port=5001)
+app.run(port=5001)                                                                    
